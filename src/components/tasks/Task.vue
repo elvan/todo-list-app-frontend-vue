62926a9037ffde73bf93e52c60c47c8569202ee0
Elvan Hidayat <elvanhd@gmail.com>
2023-07-07 04:35:52 +0700

    using computed properties

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,14 +1,19 @@
 <template>
   <li class="list-group-item py-3">
     <div class="d-flex justify-content-start align-items-center">
-      <input class="form-check-input mt-0 completed" type="checkbox" />
+      <input
+        class="form-check-input mt-0"
+        :class="completedClass"
+        type="checkbox"
+        :checked="task.is_completed"
+      />
       <div
         class="ms-2 flex-grow-1"
+        :class="completedClass"
         title="Double click the text to edit or remove"
       >
         <span>{{ task.name }}</span>
       </div>
-      <!-- <div class="task-date">24 Feb 12:00</div> -->
     </div>
     <div class="task-actions">
       <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
@@ -22,10 +27,15 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import IconPencil from '../icons/IconPencil.vue';
 import IconTrash from '../icons/IconTrash.vue';
 
-defineProps({
+const props = defineProps({
   task: Object,
 });
+
+const completedClass = computed(() =>
+  props.task.is_completed ? 'completed' : ''
+);
 </script>
